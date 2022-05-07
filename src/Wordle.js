476de53390357b65d3a6851b52cfed67c70e3bf2
Alex Biba <alexbiba@gmail.com>
2022-05-07 19:36:20 -0400

    Correct indentations

--- src/Wordle.js
+++ src/Wordle.js
@@ -2,36 +2,36 @@ import './Wordle.scss';
 import React from 'react';
 
 function Letter(props) {
-  const classes = [];
-  if (props.exact) classes.push('exact');
-  if (props.misplaced) classes.push('misplaced');
-  if (props.current) classes.push('current');
-  return (
-    <span className={['letter'].concat(classes).join(' ')}>
-      {props.char}
-    </span>
-  );
+    const classes = [];
+    if (props.exact) classes.push('exact');
+    if (props.misplaced) classes.push('misplaced');
+    if (props.current) classes.push('current');
+    return (
+        <span className={['letter'].concat(classes).join(' ')}>
+            {props.char}
+        </span>
+    );
 }
 
 function Word(props) {
-  const classes = [];
-  if (props.invalid) classes.push('invalid');
-  if (props.current) classes.push('current');
-  return (
-    <div className={['word'].concat(classes).join(' ')}>
-      {props.word.map((letter, i) => <Letter {...letter} key={i} />)}
-    </div>
-  );
+    const classes = [];
+    if (props.invalid) classes.push('invalid');
+    if (props.current) classes.push('current');
+    return (
+        <div className={['word'].concat(classes).join(' ')}>
+            {props.word.map((letter, i) => <Letter {...letter} key={i} />)}
+        </div>
+    );
 }
 
 function Wordle(props) {
-  return (
-    <div className='wordle-outer-container'>
-      <div className='wordle-inner-container'>
-        {props.words.map((word, i) => <Word word={(i === props.currWord) ? word.map((letter, j) => (j === props.currLetter) ? { ...letter, current: true } : { ...letter, current: false }) : word} current={i === props.currWord} invalid={(i === props.currWord) && props.invalidWord} key={i} />)}
-      </div>
-    </div>
-  );
+    return (
+        <div className='wordle-outer-container'>
+            <div className='wordle-inner-container'>
+                {props.words.map((word, i) => <Word word={(i === props.currWord) ? word.map((letter, j) => (j === props.currLetter) ? { ...letter, current: true } : { ...letter, current: false }) : word} current={i === props.currWord} invalid={(i === props.currWord) && props.invalidWord} key={i} />)}
+            </div>
+        </div>
+    );
 }
 
 export default Wordle;
